7031f161e51ed223045e2a9bc004d8ec5a4ba29c
Carl Crawford <carlcrawford@live.co.uk>
2016-11-07 10:45:50 +0000

    fixing the urls and sending date to apixu to fetch data

--- weatherapiproxy/urls.py
+++ weatherapiproxy/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^location/$', views.default_location, name='default_location'),
-    url(r'^location/(.*)/$', views.has_location, name='has_location'),
-    url(r'^location/(-?\d+\.\d+,-?\d+\.\d+)/start/(\d+)/$', views.date_start, name='date_start'),
-    url(r'^location/(-?\d+\.\d+,-?\d+\.\d+)/start/(\d+)/end/(\d+)/$', views.date_end, name='date_end'),
+    url(r'^location/([\w\d]+)/$', views.has_location, name='has_location'),
+    url(r'^location/([\w\d]+)/start/(\d+)/$', views.date_start, name='date_start'),
+    url(r'^location/([\w\d]+)/start/(\d+)/end/(\d+)/$', views.date_end, name='date_end'),
     url(r'^.*/$', views.api, name='api'),
 ]
--- weatherapiproxy/views.py
+++ weatherapiproxy/views.py
@@ -6,6 +6,7 @@ import os
 import ConfigParser
 
 apikey = '3569cf2928bc4988b27144752160611'
+apixu_url = 'http://api.apixu.com/v1/history.json'
 
 def index(request):
     return render(request, 'weatherapiproxy/index.html')
@@ -20,7 +21,7 @@ def default_location(request):
         'key': apikey
     }
     
-    r = requests.get('http://api.apixu.com/v1/history.json', params=payload)
+    r = requests.get(apixu_url, params=payload)
     return HttpResponse(r.text)
 
 def has_location(request, location):
@@ -29,11 +30,18 @@ def has_location(request, location):
         'dt': datetime.today().strftime('%Y-%m-%d'),
         'key': apikey
     }
-    r = requests.get('http://api.apixu.com/v1/history.json', params=payload)
+    r = requests.get(apixu_url, params=payload)
     return HttpResponse(r.text)
 
 def date_start(request, location, start):
-    return HttpResponse(start)
+    date_start = datetime.fromtimestamp(float(start))
+    payload = {
+        'q': location,
+        'dt': date_start.strftime('%Y-%m-%d'),
+        'key': apikey
+    }
+    r = requests.get(apixu_url, params=payload)
+    return HttpResponse(r.text)
 
 def date_end(request, location, start, end):
-    return HttpResponse(end)
+    return HttpResponse('The Apixu API doesn\'t support end dates - see Readme on github')
